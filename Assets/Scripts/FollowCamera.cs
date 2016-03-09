f3628c7a6479b620037c27b4e66b453ce39c82e9
mbdilaver <mbdpqw@gmail.com>
2016-03-09 21:39:56 +0200

    Initial Commit

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+
+	static public FollowCamera S;
+	public float easing = 0.05f;
+	public Vector2 minXY;
+
+
+	public bool ____________;
+
+	// poi is the bomb. Object to follow
+	public GameObject poi;
+	public float cameraZposition;
+
+	void Awake() {
+		S = this;
+		cameraZposition = this.transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (poi == null)
+			return;
+
+		Vector3 destination = poi.transform.position;
+
+		destination.x = Mathf.Max (minXY.x, destination.x);
+		destination.y = Mathf.Max (minXY.y, destination.y);
+
+		destination = Vector3.Lerp (transform.position, destination, easing);
+		destination.z = cameraZposition;
+		transform.position = destination;
+
+		this.GetComponent<Camera>().orthographicSize = destination.y + 10;
+	}
+}
---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour {
+
+	public GameObject prefabBomb;
+	public float velocityMultiplier;
+
+	// User can edit the fields above
+	public bool _______________;
+	// User should not edit the fields below
+
+	public GameObject launchPoint;
+	public Vector3 launchPosition;
+	public GameObject bomb;
+	public bool aimingMode;
+
+	void Update() {
+		if (!aimingMode)
+			return;
+
+		// Get mouse position and 
+		Vector3 mousePosition2D = Input.mousePosition;
+		mousePosition2D.z = -Camera.main.transform.position.z;
+		Vector3 mousePosition3D = Camera.main.ScreenToWorldPoint (mousePosition2D);
+
+		// Distance between launchposition and mouseposition
+		Vector3 mouseDelta = mousePosition3D - launchPosition;
+
+		float maxMagnitude = this.GetComponent<SphereCollider> ().radius;
+
+		if (mouseDelta.magnitude > maxMagnitude) 
+		{
+			mouseDelta.Normalize ();
+			mouseDelta = mouseDelta * maxMagnitude;
+		}
+
+		Vector3 bombPosition = launchPosition + mouseDelta;
+		bomb.transform.position = bombPosition;
+		if (Input.GetMouseButtonUp(0)) 
+		{
+			aimingMode = false;
+			bomb.GetComponent<Rigidbody> ().isKinematic = false;
+			bomb.GetComponent<Rigidbody> ().velocity = -mouseDelta * velocityMultiplier;
+
+			FollowCamera.S.poi = bomb;
+
+			bomb = null;
+		}
+	}
+
+	void OnMouseDown() {
+		aimingMode = true;
+		bomb = Instantiate (prefabBomb) as GameObject;
+		bomb.transform.position = launchPosition;
+		bomb.GetComponent<Rigidbody>().isKinematic = true;
+	}
+
+	void Awake() {
+		Transform launchPointTransform = transform.Find ("LaunchPoint");
+		launchPoint = launchPointTransform.gameObject;
+		launchPoint.SetActive (false);
+		launchPosition = launchPointTransform.position;
+
+	}
+
+	void OnMouseEnter() {
+		launchPoint.SetActive (true);
+	}
+
+	void OnMouseExit() {
+		launchPoint.SetActive (false);
+	}
+
+
+}
